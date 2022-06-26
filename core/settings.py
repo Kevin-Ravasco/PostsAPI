3813073dac83a739cbca126fa1730014147245fa
Kevin O <85694323+Kevin-Ravasco2@users.noreply.github.com>
2022-06-26 12:05:10 +0300

    add pagination to post list view

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,9 @@ REST_FRAMEWORK = {
         # django admin
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
 }
 
 SWAGGER_SETTINGS = {
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.views import APIView
 
 from .models import Post
 from .serializers import PostSerializer
@@ -49,5 +50,12 @@ class PostsListAPIView(ListAPIView):
     """
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
+
+
+class HandlePostLikeAPIView(APIView):
+
+    def post(self, *args, **kwargs):
+        post_id = kwargs['pk']
+
 
 
