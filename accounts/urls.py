d3a544f651740efc35b311317151d8d576db9f46
Kevin O <85694323+Kevin-Ravasco2@users.noreply.github.com>
2022-06-26 12:06:00 +0300

    update urls name

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('user/<int:id>/', views.UpdateRetrieveDeleteUserView.as_view(), name='user_details'),
+    path('user/<int:id>/', views.UpdateRetrieveDeleteUserAPIView.as_view(), name='user_details'),
     path('register/', views.RegistrationView.as_view(), name='register'),
     path('login/', swagger_auto_schema(method='post', operation_id='Login User',
                                        operation_description='Login User')(obtain_auth_token)
--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.PostsListAPIView.as_view(), name='posts'),
     path('create/', views.CreatePostAPIView.as_view(), name='create_post'),
-    path('details/<int:pk>/', views.UpdateRetrieveDeletePostAPIView.as_view(), name='create_post'),
+    path('details/<int:pk>/', views.UpdateRetrieveDeletePostAPIView.as_view(), name='post_details'),
+    path('like-unlike/<int:pk>/', views.HandlePostLikeAPIView.as_view(), name='like_unlike'),
 ]
